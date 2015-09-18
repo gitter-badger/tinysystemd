b15845eae928e51afe701c2e722fa1cdeda02f90
Like-all <like-all@yandex.com>
2015-09-18 10:23:14 +0300

    First build around v226

--- package.sh
+++ package.sh
@@ -1,8 +1,5 @@
 export DEBFULLNAME="Azer Abdullaev (Like All)"
 export DEBEMAIL="like-all@yandex.com"
-
 NAME="systemd"
-VERSION="221"
-SUFFIX="13"
-FLAVOURS="jessie"
-REBUILD="0"
+PRODUCTION_FLAVOURS="any"
+TESTING_FLAVOURS="jessie"
